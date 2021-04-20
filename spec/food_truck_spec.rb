9176aac1e946465fc4e80b4dedf0f3dc190eadb1
ahenzler <aliciahenzler@outlook.com>
2021-04-20 13:15:03 -0600

    add item test

---
+++ spec/food_truck_spec.rb
@@ -0,0 +1,24 @@
+require './lib/food_truck'
+
+RSpec.describe FoodTruck do
+  context 'exists' do
+
+    it 'is a food truck' do
+
+      expect().to be_instance_of()
+    end
+
+    it 'has attributes' do
+
+      expect().to eq()
+    end
+  end
+
+  context 'exists' do
+
+    it 'has' do
+
+      expect().to eq()
+    end
+  end
+end

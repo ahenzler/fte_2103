c542ce0cfebf3bd07a6b94effb4769978b2750fc
ahenzler <aliciahenzler@outlook.com>
2021-04-21 15:27:53 -0600

    add edited total inventory

--- lib/event.rb
+++ lib/event.rb
@@ -23,14 +23,32 @@ class Event
     end
   end
 
+  # def total_inventory
+  #   inventory = Hash.new{|h,k| h[k] = {}}
+  #   @food_trucks.each do |truck|
+  #     truck.inventory.each do |item, amount|
+  #       inventory[item] = {quantity: amount,
+  #       food_trucks: food_trucks_that_sell(item)}
+  #     end
+  #   end
+  #   inventory
+  # end
+
   def total_inventory
-    inventory = Hash.new{|h,k| h[k] = {}}
+    total_inventory = {}
     @food_trucks.each do |truck|
       truck.inventory.each do |item, amount|
-        inventory[item] = {quantity: amount, food_trucks: food_trucks_that_sell(item)}
+        if total_inventory[item].nil?
+          total_inventory[item] = {
+            quantity: 0,
+            food_trucks: []
+          }
+        end
+        total_inventory[item][:quantity] += amount
+        total_inventory[item][:food_trucks] << truck
       end
     end
-    inventory
+    total_inventory
   end
 
   def overstocked_items
